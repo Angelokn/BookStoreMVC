8ff83e2e00fee6fa4103ca2edf975a8db67dd07e
Angelo <angelo.knto@gmail.com>
2023-08-18 21:19:18 -0300

    added more fields in Register

--- ProjectWebMVC/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ ProjectWebMVC/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -108,6 +108,15 @@ namespace ProjectWeb.Areas.Identity.Pages.Account
             public string? Role { get; set; }
             [ValidateNever]
             public IEnumerable<SelectListItem> RoleList { get; set; }
+
+            [Required]
+            public string Name { get; set; }
+            public string? StreetAddress { get; set; }
+            public string? City { get; set; }
+            public string? State { get; set; }
+            public string? PostalCode { get; set; }
+            public string? PhoneNumber { get; set; }
+
         }
 
 
@@ -144,6 +153,11 @@ namespace ProjectWeb.Areas.Identity.Pages.Account
 
                 await _userStore.SetUserNameAsync(user, Input.Email, CancellationToken.None);
                 await _emailStore.SetEmailAsync(user, Input.Email, CancellationToken.None);
+                user.City = Input.City;
+                user.Name = Input.Name;
+                user.StreetAddress = Input.StreetAddress;
+                user.PhoneNumber = Input.PhoneNumber;
+                user.PostalCode = Input.PostalCode;
                 var result = await _userManager.CreateAsync(user, Input.Password);
 
                 if (result.Succeeded)
