9392543bd04e62f750b2127926f44b776af8d135
Angelo <angelo.knto@gmail.com>
2023-08-23 19:00:54 -0300

    created Shopping Cart Controller and View

---
+++ ProjectWebMVC/Areas/Customer/Controllers/CartController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ProjectWeb.Areas.Customer.Controllers
+{
+    [Area("customer")]
+    public class CartController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
