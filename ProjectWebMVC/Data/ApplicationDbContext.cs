7424745c9b0db7e9f4bda5982216e5b21026bbba
Angelo <angelo.knto@gmail.com>
2023-08-28 20:07:39 -0300

    created Model for OrderHeader and OrderDetails

--- ProjectWebMVC/Data/ApplicationDbContext.cs
+++ ProjectWebMVC/Data/ApplicationDbContext.cs
@@ -17,6 +17,9 @@ namespace ProjectWeb.Data
         public DbSet<Company> Companies { get; set; }
         public DbSet<ApplicationUser> ApplicationUsers { get; set; }
         public DbSet<ShoppingCart> ShoppingCarts { get; set; }
+        public DbSet<OrderHeader> OrderHeaders { get; set; }
+        public DbSet<OrderDetail> OrderDetails { get; set; }
+
 
 
 
---
+++ ProjectWebMVC/Models/OrderDetail.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ProjectWeb.Models
+{
+    public class OrderDetail
+    {
+        public int Id { get; set; }
+        public int OrderHeaderId { get; set; }
+        [ForeignKey("OrderHeaderId")]
+        [ValidateNever]
+        public OrderHeader OrderHeader { get; set; }
+
+        [Required]
+        public int ProductId { get; set; }
+        [ForeignKey("ProductId")]
+        [ValidateNever]
+        public Product Product { get; set; }
+
+        public int Count { get; set; }
+        public int Price { get; set; }
+    }
+}
