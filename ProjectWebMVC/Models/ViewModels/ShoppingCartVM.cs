59d64df2a8efdf624c870a16aec98e5a3ea9f7e1
Angelo <angelo.knto@gmail.com>
2023-08-29 16:25:53 -0300

    made ShoppingCartVM more dynamic

--- ProjectWebMVC/Models/ViewModels/ShoppingCartVM.cs
+++ ProjectWebMVC/Models/ViewModels/ShoppingCartVM.cs
@@ -3,7 +3,7 @@
     public class ShoppingCartVM
     {
         public IEnumerable<ShoppingCart> ShoppingCartList { get; set; }
-        public double OrderTotal { get; set; }
+        public OrderHeader OrderHeader { get; set; }
 
     }
 }
