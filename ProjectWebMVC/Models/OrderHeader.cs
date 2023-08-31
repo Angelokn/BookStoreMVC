062d661ec8f50f893777f0f1659d33a25c01e411
Angelo <angelo.knto@gmail.com>
2023-08-31 18:47:15 -0300

    created helper methods in OrderHeader repository

--- ProjectWebMVC/Models/OrderHeader.cs
+++ ProjectWebMVC/Models/OrderHeader.cs
@@ -24,6 +24,7 @@ namespace ProjectWeb.Models
         public DateTime PaymentDate { get; set; }
         public DateOnly PaymentDueDate { get; set; }
 
+        public string? SessionId { get; set; }
         public string? PaymentIntentId { get; set; }
 
         [Required]
--- ProjectWebMVC/Repository/OrderHeaderRepository.cs
+++ ProjectWebMVC/Repository/OrderHeaderRepository.cs
@@ -19,5 +19,32 @@ namespace ProjectWeb.Repository
         {
             _db.OrderHeaders.Update(obj);
         }
-    }
+
+		public void UpdateStatus(int id, string orderStatus, string? paymentStatus = null)
+		{
+			var orderFromDb = _db.OrderHeaders.FirstOrDefault(u => u.Id == id);
+            if (orderFromDb != null)
+            {
+                orderFromDb.OrderStatus = orderStatus;
+                if (!string.IsNullOrEmpty(paymentStatus))
+                {
+                    orderFromDb.PaymentStatus = paymentStatus;
+                }
+            }
+		}
+
+		public void UpdateStripePaymentId(int id, string sessionId, string paymentIntentId)
+		{
+			var orderFromDb = _db.OrderHeaders.FirstOrDefault(u => u.Id == id);
+            if (!string.IsNullOrEmpty(sessionId))
+            {
+                orderFromDb.SessionId = sessionId;
+            }
+			if (!string.IsNullOrEmpty(sessionId))
+			{
+				orderFromDb.PaymentIntentId = paymentIntentId;
+                orderFromDb.PaymentDate = DateTime.Now;
+			}
+		}
+	}
 }
