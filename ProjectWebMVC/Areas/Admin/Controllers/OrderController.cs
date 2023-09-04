aeff0c53462c0fed74190c087118a44a041ab2e6
Angelo <angelo.knto@gmail.com>
2023-09-04 18:31:06 -0300

    created Order Controller for order management

---
+++ ProjectWebMVC/Areas/Admin/Controllers/OrderController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using ProjectWeb.Models;
+using ProjectWeb.Repository.IRepository;
+
+namespace ProjectWeb.Areas.Admin.Controllers
+{
+	public class OrderController : Controller
+	{
+		private readonly IUnitOfWork _unitOfWork;
+
+        public OrderController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public IActionResult Index()
+		{
+			return View();
+		}
+
+		#region API CALLS
+
+		[HttpGet]
+		public IActionResult GetAll()
+		{
+			List<OrderHeader> objOrderHeaders = _unitOfWork.OrderHeader.GetAll(includeProperties: "ApplicationUser").ToList();
+			return Json(new { data = objOrderHeaders });
+		}
+
+		#endregion
+	}
+}
