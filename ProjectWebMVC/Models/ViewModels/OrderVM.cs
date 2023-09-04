e82b8a52f8f7ca890f751d28bfb12b06a09e9221
Angelo <angelo.knto@gmail.com>
2023-09-04 18:34:47 -0300

    created Order View Model

---
+++ ProjectWebMVC/Models/ViewModels/OrderVM.cs
@@ -0,0 +1,8 @@
+﻿namespace ProjectWeb.Models.ViewModels
+{
+	public class OrderVM
+	{
+		public OrderHeader OrderHeader { get; set; }
+		public IEnumerable<OrderDetail>	OrderDetail { get; set; }
+	}
+}
