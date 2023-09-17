4039d368bfa1b96f125909f473c66ebf65de3ae5
Angelo <angelo.knto@gmail.com>
2023-09-17 18:04:06 -0300

    created method for delayed payment - order shipped by company

--- ProjectWebMVC/Areas/Admin/Controllers/OrderController.cs
+++ ProjectWebMVC/Areas/Admin/Controllers/OrderController.cs
@@ -5,6 +5,7 @@ using ProjectWeb.Models;
 using ProjectWeb.Models.ViewModels;
 using ProjectWeb.Repository.IRepository;
 using Stripe;
+using Stripe.Checkout;
 using System.Security.Claims;
 using WebMVC.Utility;
 
@@ -124,8 +125,53 @@ namespace ProjectWeb.Areas.Admin.Controllers
             TempData["Success"] = "Order Canceled Successfully.";
             return RedirectToAction(nameof(Details), new { orderId = OrderVM.OrderHeader.Id });
         }
-            
 
+        [ActionName("Details")]
+        [HttpPost]
+        public IActionResult Detail_PAY_NOW()
+        {
+            OrderVM.OrderHeader = _unitOfWork.OrderHeader
+                .Get(u => u.Id == OrderVM.OrderHeader.Id, includeProperties: "ApplicationUser");
+            OrderVM.OrderDetail = _unitOfWork.OrderDetail
+                .GetAll(u => u.OrderHeaderId == OrderVM.OrderHeader.Id, includeProperties: "Product");
+
+            // stripe logic
+
+            var domain = "https://localhost:7154/";
+            var options = new SessionCreateOptions
+            {
+                SuccessUrl = domain + $"admin/order/PaymentConfirmation?orderHeaderId={OrderVM.OrderHeader.Id}",
+                CancelUrl = domain + $"admin/order/details?orderI={OrderVM.OrderHeader.Id}",
+                LineItems = new List<SessionLineItemOptions>(),
+                Mode = "payment",
+            };
+
+            foreach (var item in OrderVM.OrderDetail)
+            {
+                var sessionLineItem = new SessionLineItemOptions
+                {
+                    PriceData = new SessionLineItemPriceDataOptions
+                    {
+                        UnitAmount = (long)(item.Price * 100),
+                        Currency = "usd",
+                        ProductData = new SessionLineItemPriceDataProductDataOptions
+                        {
+                            Name = item.Product.Title
+                        }
+                    },
+                    Quantity = item.Count
+                };
+                options.LineItems.Add(sessionLineItem);
+            }
+
+            var service = new SessionService();
+            Session session = service.Create(options);
+            _unitOfWork.OrderHeader.UpdateStripePaymentId(OrderVM.OrderHeader.Id, session.Id, session.PaymentIntentId);
+            _unitOfWork.Save();
+            Response.Headers.Add("Location", session.Url);
+
+            return new StatusCodeResult(303);
+        }
 
         #region API CALLS
 
