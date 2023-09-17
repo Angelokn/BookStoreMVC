af22fc5efe6984207a2d4b3b589d70577d5e8975
Angelo <angelo.knto@gmail.com>
2023-09-17 17:17:31 -0300

    created Cancel order method for Stripe

--- ProjectWebMVC/Areas/Admin/Controllers/OrderController.cs
+++ ProjectWebMVC/Areas/Admin/Controllers/OrderController.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Mvc;
 using ProjectWeb.Models;
 using ProjectWeb.Models.ViewModels;
 using ProjectWeb.Repository.IRepository;
+using Stripe;
 using System.Security.Claims;
 using WebMVC.Utility;
 
@@ -94,6 +95,36 @@ namespace ProjectWeb.Areas.Admin.Controllers
             TempData["Success"] = "Order Shipped Successfully.";
             return RedirectToAction(nameof(Details), new { orderId = OrderVM.OrderHeader.Id });
         }
+
+        [HttpPost]
+        [Authorize(Roles = SD.Role_Admin + "," + SD.Role_Employee)]
+        public IActionResult CancelOrder()
+        {
+            var orderHeader = _unitOfWork.OrderHeader.Get(u => u.Id == OrderVM.OrderHeader.Id);
+
+            if (orderHeader.PaymentStatus == SD.PaymentStatusApproved)
+            {
+                var options = new RefundCreateOptions
+                {
+                    Reason = RefundReasons.RequestedByCustomer,
+                    PaymentIntent = orderHeader.PaymentIntentId
+                };
+
+                var service = new RefundService();
+                Refund refund = service.Create(options);
+
+                _unitOfWork.OrderHeader.UpdateStatus(orderHeader.Id, SD.StatusCancelled, SD.StatusRefunded);
+            }
+            else
+            {
+                _unitOfWork.OrderHeader.UpdateStatus(orderHeader.Id, SD.StatusCancelled, SD.StatusCancelled);
+            }
+
+            _unitOfWork.Save();
+            TempData["Success"] = "Order Canceled Successfully.";
+            return RedirectToAction(nameof(Details), new { orderId = OrderVM.OrderHeader.Id });
+        }
+            
 
         #region API CALLS
 
