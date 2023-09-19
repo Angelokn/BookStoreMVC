237467cf1c634361f6bdae70ddaf7b86b3d75cc3
Angelo <angelo.knto@gmail.com>
2023-09-19 18:27:02 -0300

    Shopping cart now shows the correct value when the user logs in

--- ProjectWebMVC/Areas/Customer/Controllers/HomeController.cs
+++ ProjectWebMVC/Areas/Customer/Controllers/HomeController.cs
@@ -23,6 +23,15 @@ namespace ProjectWeb.Areas.Customer.Controllers
 
         public IActionResult Index()
         {
+            var claimsIdentity = (ClaimsIdentity)User.Identity;
+            var claim = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier);
+
+            if (claim != null)
+            {
+                HttpContext.Session.SetInt32(SD.SessionCart,
+                _unitOfWork.ShoppingCart.GetAll(u => u.ApplicationUserId == claim.Value).Count());
+            }
+
             IEnumerable<Product> productList = _unitOfWork.Product.GetAll(includeProperties: "Category");
             return View(productList);
         }
@@ -63,7 +72,7 @@ namespace ProjectWeb.Areas.Customer.Controllers
                 _unitOfWork.ShoppingCart.Add(shoppingCart);
                 _unitOfWork.Save();
                 HttpContext.Session.SetInt32(SD.SessionCart,
-                    _unitOfWork.ShoppingCart.GetAll(u => u.ApplicationUserId == userId).Count());
+                _unitOfWork.ShoppingCart.GetAll(u => u.ApplicationUserId == userId).Count());
             }
 
             TempData["success"] = "Cart updated successfully";
