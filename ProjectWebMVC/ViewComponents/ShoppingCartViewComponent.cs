2c86c32dfec03006309ac5a7a313f7930a84161c
Angelo <angelo.knto@gmail.com>
2023-09-21 19:29:27 -0300

    created backend async task for SC view component

--- ProjectWebMVC/ViewComponents/ShoppingCartViewComponent.cs
+++ ProjectWebMVC/ViewComponents/ShoppingCartViewComponent.cs
@@ -12,5 +12,27 @@ namespace ProjectWeb.ViewComponents
         {
             _unitOfWork = unitOfWork;
         }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var claimsIdentity = (ClaimsIdentity)User.Identity;
+            var claim = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier);
+
+            if (claim != null)
+            {
+                if (HttpContext.Session.GetInt32(SD.SessionCart) == null)
+                {
+                    HttpContext.Session.SetInt32(SD.SessionCart,
+                    _unitOfWork.ShoppingCart.GetAll(u => u.ApplicationUserId == claim.Value).Count());
+                }                
+                return View(HttpContext.Session.GetInt32(SD.SessionCart));
+            }
+            else
+            {
+                HttpContext.Session.Clear();
+
+                return View(0);
+            }
+        }
     }
 }
