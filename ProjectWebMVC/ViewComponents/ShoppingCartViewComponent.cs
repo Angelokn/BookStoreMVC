4d624a9cdcaaf010e13f0decbc1ea193a8db3003
Angelo <angelo.knto@gmail.com>
2023-09-21 19:28:25 -0300

    created ShoppingCartViewComponent

---
+++ ProjectWebMVC/ViewComponents/ShoppingCartViewComponent.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProjectWeb.Repository.IRepository;
+using System.Security.Claims;
+using WebMVC.Utility;
+
+namespace ProjectWeb.ViewComponents
+{
+    public class ShoppingCartViewComponent : ViewComponent
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        public ShoppingCartViewComponent(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+    }
+}
