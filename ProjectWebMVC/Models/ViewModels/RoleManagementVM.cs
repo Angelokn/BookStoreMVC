837da10d479723f47ee715796a8532c5ae2edebf
Angelo <angelo.knto@gmail.com>
2023-10-03 18:01:49 -0300

    created Role management view model

---
+++ ProjectWebMVC/Models/ViewModels/RoleManagementVM.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace ProjectWeb.Models.ViewModels
+{
+    public class RoleManagementVM
+    {
+        public ApplicationUser ApplicationUser { get; set; }
+        public IEnumerable<SelectListItem> RoleList { get; set; }
+        public IEnumerable<SelectListItem> CompanyList { get; set; }
+    }
+}
