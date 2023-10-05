82eff18bbafa38cc07f1d2771e85c1cba38410c3
Angelo <angelo.knto@gmail.com>
2023-10-05 20:26:07 -0300

    added migration to remove ImageUrl from Products table

--- ProjectWebMVC/DataAccess/Migrations/20231005232424_removeImageUrl.Designer.cs
+++ ProjectWebMVC/DataAccess/Migrations/20231005232424_removeImageUrl.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using ProjectWeb.DataAccess.Data;
 
@@ -11,9 +12,11 @@ using ProjectWeb.DataAccess.Data;
 namespace ProjectWeb.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20231005232424_removeImageUrl")]
+    partial class removeImageUrl
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
