ad625b4f3ab3e809ae506d4630c37e4198d7d40e
Angelo <angelo.knto@gmail.com>
2023-10-06 17:18:52 -0300

    created ProductImage model

---
+++ ProjectWebMVC/Models/ProductImage.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ProjectWeb.Models
+{
+    public class ProductImage
+    {
+        public int Id { get; set; }
+        [Required]
+        public string ImageUrl { get; set; }
+        public int ProductId { get; set; }
+        [ForeignKey("ProductId")]
+        public Product Product { get; set; }
+    }
+}
