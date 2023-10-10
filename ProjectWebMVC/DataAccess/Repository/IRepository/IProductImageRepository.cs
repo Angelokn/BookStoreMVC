f4bcc7eb7b5331eb0b095796af79329e5b3d7341
Angelo <angelo.knto@gmail.com>
2023-10-10 18:50:52 -0300

    created IProductImageRepository

---
+++ ProjectWebMVC/DataAccess/Repository/IRepository/IProductImageRepository.cs
@@ -0,0 +1,9 @@
+﻿using ProjectWeb.Models;
+
+namespace ProjectWeb.DataAccess.Repository.IRepository
+{
+    public interface ICategoryRepository : IRepository<Category>
+    {
+        void Update(Category obj);
+    }
+}
