a9a601edda8e383ac435f78c8837a0fc1657618c
Angelo <angelo.knto@gmail.com>
2023-10-10 18:54:44 -0300

    created ProductImageRepository

---
+++ ProjectWebMVC/DataAccess/Repository/ProductImageRepository.cs
@@ -0,0 +1,22 @@
+﻿using ProjectWeb.DataAccess.Data;
+using ProjectWeb.DataAccess.Repository.IRepository;
+using ProjectWeb.Models;
+using System.Linq.Expressions;
+
+namespace ProjectWeb.DataAccess.Repository
+{
+    public class ProductImageRepository : Repository<ProductImage>, IProductImageRepository
+    {
+        private ApplicationDbContext _db;
+
+        public ProductImageRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+
+        public void Update(ProductImage obj)
+        {
+            _db.ProductImages.Update(obj);
+        }
+    }
+}
