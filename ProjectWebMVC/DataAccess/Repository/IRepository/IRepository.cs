038301e85e08c202bdbb3d5aa7d3d57ae0932fc9
Angelo <angelo.knto@gmail.com>
2023-11-07 19:30:57 -0300

    commented depreciated nuget package

--- ProjectWebMVC/DataAccess/Repository/IRepository/IRepository.cs
+++ ProjectWebMVC/DataAccess/Repository/IRepository/IRepository.cs
@@ -4,7 +4,6 @@ namespace ProjectWeb.DataAccess.Repository.IRepository
 {
     public interface IRepository<T> where T : class
     {
-        //T - Category
         IEnumerable<T> GetAll(Expression<Func<T, bool>>? filter = null, string? includeProperties = null);
         T Get(Expression<Func<T, bool>> filter, string? includeProperties = null, bool tracked = false);
         void Add(T entity);
